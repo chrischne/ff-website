d1dffae20d0f2b3ca2fb842a5687301b162e8b5e
chrischne <chrischne@gmail.com>
2019-09-01 22:00:39 +0200

    add a button

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,28 @@
 <template>
-  <section class="section edo-theme">
-    <h1 class="hello-nuxt">Hello Nuxt!</h1>
-  </section>
+  <div>
+    <section class="section edo-theme">
+      <h1 class="hello-nuxt">Hello Nuxt!</h1>
+    </section>
+    <section class="section">
+      <a
+        class="button is-primary is-rounded"
+        @click="doClick"
+      >Rounded</a>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-  components: {}
+  components: {},
+  methods: {
+    doClick () {
+      console.log('hi')
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-@import '~/assets/scss/main.scss';
+@import "~/assets/scss/main.scss";
 </style>
