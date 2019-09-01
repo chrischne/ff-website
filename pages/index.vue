5486b01d51df3e577b490256f12cdf632251d1cb
chrischne <chrischne@gmail.com>
2019-09-01 21:54:20 +0200

    initial

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <section class="section edo-theme">
+    <h1 class="hello-nuxt">Hello Nuxt!</h1>
+  </section>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/main.scss';
+</style>
