06dfe3202bf8c769c08e2fba599b3ba0e959dd06
chrischne <chrischne@gmail.com>
2019-09-03 22:24:13 +0200

    add sidebar and books

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,58 @@
 <template>
   <div>
+    <nav class="navbar has-shadow">
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          <img src="https://robohash.org/bumble" alt="">
+        </a>
+        <div class="navbar-burger">
+          <span />
+          <span />
+          <span />
+        </div>
+        <!-- Logo, tagline, and navbar-burger -->
+      </div>
+
+      <div class="navbar-menu">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <small>Publishing at the speed of technology</small>
+          </div>
+        </div>
+        <div class="navbar-end">
+          <div class="navbar-item has-dropdown is-hoverable">
+            <div class="navbar-link">
+              Alex Johnson
+            </div>
+            <div class="navbar-dropdown">
+              <a class="navbar-item">
+                <div>
+                  <span class="icon is-small">
+                    <i class="fa fa-user-circle-o" />
+                  </span>
+                  Profile</div>
+              </a>
+              <a class="navbar-item">
+                <div>
+                  <span class="icon is-small">
+                    <i class="fa fa-bug" />
+                  </span>
+                  Report bug
+                </div>
+              </a>
+              <a class="navbar-item">
+                <div>
+                  <span class="icon is-small">
+                    <i class="fa fa-sign-out" />
+                  </span>
+                  Sign Out
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
     <nuxt />
   </div>
 </template>
---
+++ pages/books.vue
@@ -0,0 +1,111 @@
+<template>
+  <section class="section">
+    <div class="columns">
+      <div class="column is-4-tablet is-3-desktop is-2-widescreen">
+        <nav class="menu">
+          <p class="menu-label">
+            Menu
+          </p>
+          <ul class="menu-list">
+            <li>
+              <a href="dashboard.html">
+                <span class="icon">
+                  <i class="fa fa-tachometer" />
+                </span>
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a class="is-active" href="books.html">
+                <span class="icon">
+                  <i class="fa fa-book" />
+                </span>
+                Books
+              </a>
+            </li>
+            <li>
+              <a href="customers.html">
+                <span class="icon">
+                  <i class="fa fa-address-book" />
+                </span>
+                Customers
+              </a>
+            </li>
+            <li>
+              <a href="orders.html">
+                <span class="icon">
+                  <i class="fa fa-file-text-o" />
+                </span>
+                Orders
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="column">
+        <h1 class="title">
+          Books
+        </h1>
+
+        <nav class="level">
+          <div class="level-left">
+            <div class="level-item">
+              <p class="subtitle is-5">
+                <strong>6</strong> books
+              </p>
+            </div>
+
+            <p class="level-item">
+              <a class="button is-success" href="new-book.html">New</a>
+            </p>
+
+            <div class="level-item is-hidden-tablet-only">
+              <div class="field has-addons">
+                <p class="control">
+                  <input class="input" type="text" placeholder="Book name, ISBN…">
+                </p>
+                <p class="control">
+                  <button class="button">
+                    Search
+                  </button>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="level-right">
+            <div class="level-item">
+              Order by
+            </div>
+            <div class="level-item">
+              <div class="select">
+                <select>
+                  <option>Publish date</option>
+                  <option>Price</option>
+                  <option>Page count</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+
+@import '@/assets/scss/main.scss';
+
+.section{
+  border: 5px solid green;
+}
+
+.column{
+  border: 1px solid black;
+}
+</style>
