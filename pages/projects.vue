cabdd971d086c12144c75470ab2a2c493aa57aa3
chrischne <chrischne@gmail.com>
2019-09-09 21:54:10 +0200

    create card

---
+++ pages/projects.vue
@@ -0,0 +1,42 @@
+<template>
+  <section class="section">
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <!-- <div class="media-left">
+            <figure class="image is-48x48">
+              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+            </figure>
+          </div> -->
+          <div class="media-content">
+            <p class="title is-4">
+              EEG momol
+            </p>
+            <p class="subtitle is-6">
+              Portable EEG Monitoring
+            </p>
+          </div>
+        </div>
+
+        <div class="content">
+          Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue.
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
