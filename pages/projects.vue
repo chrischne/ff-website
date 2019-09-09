3f1129a93097dc9221c0319800e57f6d9d5c01cf
chrischne <chrischne@gmail.com>
2019-09-09 22:10:50 +0200

    create ProjectCard component

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,39 +1,21 @@
 <template>
   <section class="section">
-    <div class="card">
-      <div class="card-image">
-        <figure class="image is-4by3">
-          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-        </figure>
-      </div>
-      <div class="card-content">
-        <div class="media">
-          <!-- <div class="media-left">
-            <figure class="image is-48x48">
-              <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-            </figure>
-          </div> -->
-          <div class="media-content">
-            <p class="title is-4">
-              EEG momol
-            </p>
-            <p class="subtitle is-6">
-              Portable EEG Monitoring
-            </p>
-          </div>
-        </div>
-
-        <div class="content">
-          Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue.
-        </div>
-      </div>
-    </div>
+    <ProjectCard title="TheTitle" subtitle="thesubtitle" content="thecontent" />
   </section>
 </template>
 
 <script>
-export default {
+import ProjectCard from '@/components/ProjectCard'
 
+export default {
+  components: {
+    ProjectCard
+  },
+  data () {
+    return {
+      greeting: 'hello'
+    }
+  }
 }
 </script>
 
