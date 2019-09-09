9d726ffef11ea8b977cd3f6e7a6a8bfc54544489
chrischne <chrischne@gmail.com>
2019-09-09 23:19:26 +0200

    add link to project

---
+++ components/NavBar.vue
@@ -0,0 +1,59 @@
+<template>
+  <nav class="navbar has-shadow is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="/">
+        <img class="navbar-item" src="logo3.png">
+      </a>
+      <div class="navbar-item has-text-weight-bold is-size-2 is-uppercase narrow">
+        <!-- The ReSEARCH Lab -->
+      </div>
+      <a
+        role="button"
+        class="navbar-burger burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbarBasicExample"
+        :class="{'is-active': isOpen}"
+        @click="isOpen = !isOpen"
+      >
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+        <span aria-hidden="true" />
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': isOpen}">
+      <div class="navbar-end">
+        <nuxt-link class="navbar-item has-text-weight-semibold is-size-6a is-uppercase" to="/">
+          Home
+        </nuxt-link>
+        <nuxt-link class="navbar-item has-text-weight-semibold is-size-6a is-uppercase" to="/projects">
+          Projects
+        </nuxt-link>
+
+        <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
+          People
+        </a>
+        <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
+          About
+        </a>
+        <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
+          Join
+        </a>
+        <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
+          Contact
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,6 @@
 <template>
   <div>
-    <nav class="navbar has-shadow is-fixed-top" role="navigation" aria-label="main navigation">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="/">
-          <img class="navbar-item" src="logo3.png">
-        </a>
-        <div class="navbar-item has-text-weight-bold is-size-2 is-uppercase narrow">
-          <!-- The ReSEARCH Lab -->
-        </div>
-        <a
-          role="button"
-          class="navbar-burger burger"
-          aria-label="menu"
-          aria-expanded="false"
-          data-target="navbarBasicExample"
-          :class="{'is-active': isOpen}"
-          @click="isOpen = !isOpen"
-        >
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-          <span aria-hidden="true" />
-        </a>
-      </div>
-
-      <div id="navbarBasicExample" class="navbar-menu" :class="{'is-active': isOpen}">
-        <div class="navbar-end">
-          <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
-            Home
-          </a>
-          <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
-            Projects
-          </a>
-          <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
-            People
-          </a>
-          <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
-            About
-          </a>
-          <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
-            Join
-          </a>
-          <a class="navbar-item has-text-weight-semibold is-size-6a is-uppercase">
-            Contact
-          </a>
-        </div>
-      </div>
-    </nav>
+    <NavBar />
     <nuxt />
     <footer class="footer">
       <div class="content">
@@ -57,7 +12,11 @@
   </div>
 </template>
 <script>
+import NavBar from '@/components/NavBar'
 export default {
+  components: {
+    NavBar
+  },
   data () {
     return {
       isOpen: false
