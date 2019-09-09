dce2228e8275932dac10c6e1936653e83cfe7b9d
chrischne <chrischne@gmail.com>
2019-09-09 22:45:31 +0200

    arrange ProjectCard

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,6 +1,10 @@
 <template>
   <section class="section">
-    <ProjectCard title="TheTitle" subtitle="thesubtitle" content="thecontent" />
+    <div class="columns is-multiline">
+      <div v-for="(project,index) in projects" :key="index" class="column is-6-tablet is-6-desktop is-4-widescreen">
+        <ProjectCard :title="project.title" :subtitle="project.subtitle" :content="project.content" />
+      </div>
+    </div>
   </section>
 </template>
 
@@ -13,7 +17,29 @@ export default {
   },
   data () {
     return {
-      greeting: 'hello'
+      greeting: 'hello',
+      projects: [
+        {
+          title: 'MobiEEG',
+          subtitle: 'Mobile EEG Monitoring',
+          content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue.'
+        },
+        {
+          title: 'Stress Level',
+          subtitle: 'Stress Lebel Monitoring at work',
+          content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue. '
+        },
+        {
+          title: 'Neurodesign',
+          subtitle: 'Parametric Design based on Biofeedback',
+          content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue. '
+        },
+        {
+          title: 'BigViz',
+          subtitle: 'Big Data Visualisation',
+          content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue. '
+        }
+      ]
     }
   }
 }
