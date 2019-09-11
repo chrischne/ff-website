d50520949e4e4c4bf56fe7d22ca3e416ca7bf986
chrischne <chrischne@gmail.com>
2019-09-11 18:26:15 +0200

    fixed image url related stuff

---
+++ pages/imagetest.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <img :src="image" :alt="selectedDog">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      image: '/projectimages/image1.jpg'
+    }
+  },
+  computed: {
+    imageUrl () {
+      return require(this.image)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- pages/projects/_id.vue
+++ pages/projects/_id.vue
@@ -1,10 +1,11 @@
 <template>
   <section class="section">
-    <div class="columns is-multiline">
+    <!-- <div class="columns is-multiline">
       <div v-for="(project,index) in projects" :key="index" class="column is-6-tablet is-6-desktop is-4-widescreen">
         <ProjectCard :title="project.title" :subtitle="project.subtitle" :content="project.content" :image="project.image" />
       </div>
-    </div>
+    </div> -->
+    <ProjectCard :title="project.title" :subtitle="project.subtitle" :content="project.content" :image="project.image" />
   </section>
 </template>
 
@@ -17,33 +18,42 @@ export default {
   },
   data () {
     return {
-      greeting: 'hello',
+      id: this.$route.params.id,
       projects: [
         {
+          id: 'mobieeg',
           title: 'MobiEEG',
           subtitle: 'Mobile EEG Monitoring',
           content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue.',
-          image: 'projectimages/image1.jpg'
+          image: '/projectimages/image1.jpg'
         },
         {
+          id: 'stresslevel',
           title: 'Stress Level',
           subtitle: 'Stress Level Monitoring at work',
           content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue. ',
-          image: 'projectimages/image2.jpg'
+          image: '/projectimages/image2.jpg'
         },
         {
+          id: 'neurodesign',
           title: 'Neurodesign',
           subtitle: 'Parametric Design based on Biofeedback',
           content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue. ',
-          image: 'projectimages/image3.jpg'
+          image: '/projectimages/image3.jpg'
         },
         {
+          id: 'bigviz',
           title: 'BigViz',
           subtitle: 'Big Data Visualisation',
           content: 'Maiden Flight is an autonomous biological laboratory environment designed for studying the impact of space flight on the sole reproductive node of a bee colony: the queen bee and her retinue. ',
-          image: 'projectimages/image4.jpg'
+          image: '/projectimages/image4.jpg'
         }
       ]
+    }
+  },
+  computed: {
+    project () {
+      return this.projects.find(p => p.id === this.id)
     }
   }
 }
