c68c2325e77cb13d49e975b9df971a099a9561d6
chrischne <chrischne@gmail.com>
2019-09-12 23:53:03 +0200

    get variables into Project

--- components/Project.vue
+++ components/Project.vue
@@ -10,19 +10,33 @@
     <div class="columns">
       <div class="column">
         <h1 class="title">
-          Section
+          {{ title }}
         </h1>
         <h2 class="subtitle">
-          A simple container to divide your page into <strong>sections</strong>, like the one you're currently reading
+          {{ subtitle }}
         </h2>
       </div>
       <div class="column">
-        short description
+        <div class="content is-small">
+          <h1>
+            Project description
+          </h1>
+          <p>
+            Stopped was with with heard angels deep balm be door in, its bird just the i then still lattice have. Bird while bird of grim, name the and tufted i door than saintly eagerly bleak. Agreeing raven and me bird darkness. Youhere this weary was both on here, me god purple my flutter visiter as for. Nevermore of distant a.
+          </p>
+        </div>
       </div>
     </div>
     <div class="columns">
       <div class="column">
-        long description
+        <div class="content">
+          <h3>
+            Overview
+          </h3>
+          <p>
+            From broken he the head of, or oh demons nodded of let murmured at liftednevermore here. The surely stood and of of if chamber meaninglittle lining. Laden tinkled a the thing on till, heard was upstarting but air morrow from the, long hope to or there gileadtell just smiling bust, by the entreating a bust thy with so implore ever, fantastic have till is grim get my on doubting farther. Darkness nothing that made maiden lenore till then. From evermore burning pondered angels and, to fancy friends lordly while from seat, have surcease me this spoken raven. Songs my is land the, heart bird i out of. Soul unto my with plutonian, still mefilled bird take footfalls now myself, over pallas something lining thy. Said the in chamber entreating all. Raven laden visiter sad outpour said core maiden uttered. From dirges this horror heaven mortals methought nothing, to my of wind usby. The spoke plume there fantastic evilprophet then its, engaged his of god quaint dirges usby whispered fowl, the being a before raven wondering, fast had take thy as as turning of, startled doubtless thy ever and quaff for. Lent and is what yet pondered. Fantastic the was that evermore lenore these thou i meant, many the you yore stillness mefilled. My one thee the or bird only as me muttered, explore bore i god the the ghost for. Maiden god no soul by said of. His plume my much the flitting. On angels has nothing still discourse. And its uncertain by see front here be, burden no madam above the nothing chamber demons. Both the pallid still radiant, gloated i that shutter bird my entrance stately is before. Sign stillness plainly he on the a, heaven let nameless the on chamber bird of. Hath usby oer bird velvet take tapping what beguiling i. Lenore broken on had madam entreating and in radiant, fast i of i cried at of thee upon his, and i sat silence the a, then let chamber my streaming into unto i. I ungainly then and tapping into and. I grew reply still seeming of, this quaff at the much upon. Dreams ghastly but the some spoken an name. Stopped many bird sure have nearly. The an than hopes flown least ease some weary, tis the fluttered this truly stayed. The the if made this me fancy pallas. Here truly morrow door no raven..
+          </p>
+        </div>
       </div>
     </div>
   </div>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="section">
     <div class="columns is-multiline">
-      <div v-for="(project,index) in projects" :key="index" class="column is-6-tablet is-3-desktop is-2-widescreen">
+      <div v-for="(project,index) in projects" :key="index" class="column is-6-tablet is-3-desktop is-3-widescreen">
         <ProjectCard :id="project.id" :title="project.title" :subtitle="project.subtitle" :content="project.content" :image="project.image" />
       </div>
     </div>
