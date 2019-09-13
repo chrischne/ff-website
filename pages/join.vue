04a7783c0a55ca0579552d5f3a3554a01ad09d30
chrischne <chrischne@gmail.com>
2019-09-13 21:58:29 +0200

    Create join.vue

---
+++ pages/join.vue
@@ -0,0 +1,17 @@
+<template>
+  <section class="section">
+    <div class="container">
+      here comes a membership form or some kind of third party subscription thing
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
