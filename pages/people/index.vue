a77d236fca85523cd3f750560318de22bdd53d79
chrischne <chrischne@gmail.com>
2019-11-19 19:24:55 +0100

    sort people

--- pages/people/index.vue
+++ pages/people/index.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="container">
       <div class="columns is-multiline">
-        <div v-for="(person) in people" :key="person.id" class="column is-4-tablet is-3-desktop is-3-widescreen">
+        <div v-for="(person) in sortedPeople" :key="person.id" class="column is-4-tablet is-3-desktop is-3-widescreen">
           <PersonCard :id="person.id" :name="person.name" :role="person.role" :image="person.image" />
         </div>
       </div>
@@ -24,6 +24,10 @@ export default {
   computed: {
     people () {
       return this.$store.state.people[this.locale].all
+    },
+    sortedPeople () {
+      const arr = []
+      return this.people.slice().sort((a, b) => a.sort - b.sort)
     },
     locale () {
       return this.$i18n.locale
