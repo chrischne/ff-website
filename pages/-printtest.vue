6684ea16302c24dc942109630388e51e194251a2
chrischne <chrischne@gmail.com>
2019-11-20 22:37:27 +0100

    draft

---
+++ pages/-printtest.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="columns is-multiline">
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <figure class="image is-4by3">
+            <img src="https://source.unsplash.com/800x600/?forest">
+          </figure>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <div class="content">
+            <p>
+              Gefärbt zürnen bist dahinten im mut dich dich jedoch weh verschwand. Dich geliebet mal in mein schnee nicht du es oft, komm um verwundert wo weh klingt weiter gartens und wiedersehn. Wo mir's und schnelle des bäume du oft. Mund du treibt du beiden so es wo du denkst, komm.
+            </p>
+          </div>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <figure class="image is-4by3">
+            <img src="https://source.unsplash.com/800x600/?forest">
+          </figure>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <div class="content">
+            <p>
+              Gefärbt zürnen bist dahinten im mut dich dich jedoch weh verschwand. Dich geliebet mal in mein schnee nicht du es oft, komm um verwundert wo weh klingt weiter gartens und wiedersehn. Wo mir's und schnelle des bäume du oft. Mund du treibt du beiden so es wo du denkst, komm.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="section">
+      <div class="columns is-multiline">
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <figure class="image is-4by3">
+            <img src="https://source.unsplash.com/800x600/?forest">
+          </figure>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <div class="content">
+            <p>
+              Gefärbt zürnen bist dahinten im mut dich dich jedoch weh verschwand. Dich geliebet mal in mein schnee nicht du es oft, komm um verwundert wo weh klingt weiter gartens und wiedersehn. Wo mir's und schnelle des bäume du oft. Mund du treibt du beiden so es wo du denkst, komm.
+            </p>
+          </div>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <figure class="image is-4by3">
+            <img src="https://source.unsplash.com/800x600/?forest">
+          </figure>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <div class="content">
+            <p>
+              Gefärbt zürnen bist dahinten im mut dich dich jedoch weh verschwand. Dich geliebet mal in mein schnee nicht du es oft, komm um verwundert wo weh klingt weiter gartens und wiedersehn. Wo mir's und schnelle des bäume du oft. Mund du treibt du beiden so es wo du denkst, komm.
+            </p>
+          </div>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <div class="content">
+            <p>
+              Gefärbt zürnen bist dahinten im mut dich dich jedoch weh verschwand. Dich geliebet mal in mein schnee nicht du es oft, komm um verwundert wo weh klingt weiter gartens und wiedersehn. Wo mir's und schnelle des bäume du oft. Mund du treibt du beiden so es wo du denkst, komm.
+            </p>
+          </div>
+        </div>
+        <div class="column is-6-tablet is-3-desktop is-3-widescreen">
+          <figure class="image is-4by3">
+            <img src="https://source.unsplash.com/800x600/?forest">
+          </figure>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+/* .column{
+    border: 1px solid black;
+} */
+
+section {
+    page-break-before: always;
+}
+/* .column{
+    page-break-before: auto;
+    page-break-inside: avoid;
+    page-break-after: unset;
+} */
+
+</style>
