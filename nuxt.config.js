98ec707d311e03e843928d491c8fb4c797b7fb66
chrischne <chrischne@gmail.com>
2019-11-20 19:29:31 +0100

    Adding travis deploy configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,10 @@
+/* nuxt.config.js */
+// only add `router.base = '/<repository-name>/'` if `DEPLOY_ENV` is `GH_PAGES`
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/<repository-name>/'
+  }
+} : {}
 
 export default {
   mode: 'universal',
@@ -10,7 +17,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { hid: 'robots', name: 'robots', content: 'noindex' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -112,5 +120,6 @@ export default {
         }
       }
     }
-  }
+  },
+  ...routerBase
 }
