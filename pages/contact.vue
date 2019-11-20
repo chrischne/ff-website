23819f036ddba3ebd92ec0ceb54c969176788ee7
chrischne <chrischne@gmail.com>
2019-11-20 22:37:48 +0100

    Update contact.vue

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,44 +4,14 @@
       <div class="columns">
         <div class="column">
           <AddressCard
-            title="Inquiries"
-            name="Françoise Lustenberger"
-            role="Administrative Assistant"
-            institution="Unabhängige Forschungsstelle Bern"
+            :title="$t('contact')"
+            name="Freie Forschung"
             street="Bahnstrasse 44"
             zip="CH-3008"
             city="Bern"
             country="Switzerland"
-            phone="+41 123 45 67"
-            email="ping@pong.ch"
-          />
-        </div>
-        <div class="column">
-          <AddressCard
-            title="Talks"
-            name="Hanspeter Hofmann"
-            role="Administrative Assistant"
-            institution="Unabhängige Forschungsstelle Bern"
-            street="Bahnstrasse 44"
-            zip="CH-3008"
-            city="Bern"
-            country="Switzerland"
-            phone="+41 123 45 67"
-            email="ping@pong.ch"
-          />
-        </div>
-        <div class="column">
-          <AddressCard
-            title="Press"
-            name="Jacqueline Schneider"
-            role="Administrative Assistant"
-            institution="Unabhängige Forschungsstelle Bern"
-            street="Bahnstrasse 44"
-            zip="CH-3008"
-            city="Bern"
-            country="Switzerland"
-            phone="+41 123 45 67"
-            email="ping@pong.ch"
+            phone="-"
+            email="info@ffff.io"
           />
         </div>
       </div>
