e499ab90849d955dcdbb1700592dbff5988549d4
chrischne <chrischne@gmail.com>
2019-11-22 14:59:02 +0100

    layouting

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -2,7 +2,7 @@
   <section class="section">
     <div class="container">
       <div class="columns is-multiline">
-        <div v-for="(project,index) in projects" :key="index" class="column is-6-tablet is-4-desktop is-3-widescreen">
+        <div v-for="(project,index) in projects" :key="index" class="column is-6-tablet is-4-desktop is-4-widescreen">
           <ProjectCard :id="project.id" :title="project.title" :subtitle="project.subtitle" :content="project.content" :image="project.image" />
         </div>
       </div>
